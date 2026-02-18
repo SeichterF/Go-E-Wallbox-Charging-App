df809d2ff8e8ce63bb17d239703f4a4aa9a5a8e8
Florian Seichter <fseichter@icloud.com>
2026-02-18 21:19:05 +0100

    Initial Commit

---
+++ Walbox LimitTests/Walbox_LimitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Walbox_LimitTests.swift
+//  Walbox LimitTests
+//
+//  Created by Florian Seichter on 18.02.26.
+//
+
+import Testing
+@testable import Walbox_Limit
+
+struct Walbox_LimitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
